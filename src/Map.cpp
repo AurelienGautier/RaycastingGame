9ce931b4c38589323fa6808ff5bb684b5b1f4842
aurelien22 <aurelien22@brouetteRapide.home>
2023-07-03 21:34:22 +0200

    [Fix] The game do not crash anymore when the player touches a corner but he's brought back too far behind

--- src/Map.cpp
+++ src/Map.cpp
@@ -81,8 +81,8 @@ void Map::movePlayer(std::shared_ptr<Player> player, std::string direction)
 	else 
 	{
 		player->setPosition(
-			round(playerPosition.x) * this->cellSize,
-			round(playerPosition.y) * this->cellSize
+			round(player->getPosition().x / this->cellSize) * this->cellSize,
+			round(player->getPosition().y / this->cellSize) * this->cellSize
 		);
 	}
 }
